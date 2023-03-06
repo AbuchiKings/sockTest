a47eb3509e243f8d81eed3e2d39f7edb34d2c840
AbuchiKings <abuchikingsley76@gmail.com>
2023-03-06 13:16:06 +0100

    Initial

--- src/components/Sender.js
+++ src/components/Sender.js
@@ -3,7 +3,7 @@ import socketio from 'socket.io-client'
 
 
 const ENDPOINT = 'https://app.scanpay.ng'
-const ENDPOINT1 = 'http://localhost:8005'
+const ENDPOINT1 = 'http://localhost:8000/users'
 
 const Sender = () => {
     const ref = useRef({
@@ -15,14 +15,19 @@ const Sender = () => {
         })
     })
     useEffect(() => {
-        ref.current.socket.emit('addUser', {
-            userId: '6072064d8fed772c94d25707', category: "sender", role: 'user',
-            reference: 'POS-fb66c64a470b9b4cd516a2d26523ddd3$6072064d8fed772c94d25707'
+        ref.current.socket.emit('user:join', {
+            "user_id": "634d3f9c5f6c3a4a9014effe", txn_reference:'FND-56VuFM3Hp7cCG1AD1673960411912'
         });
         return () => ref.current.socket.disconnect()
     }, []);
 
-    ref.current.socket.on('joined', data => {
+    ref.current.socket.on('user:joined', data => {
+        console.log(data)
+    })
+    ref.current.socket.on('user:transaction:update', data => {
+        console.log(data)
+    })
+    ref.current.socket.on('user:join:failed', data => {
         console.log(data)
     })
 
